96a90da2db029b6433d10cdfcdaacd7ae3af226b
achoo-o <amyj.etana@gmail.com>
2024-08-31 18:45:02 +1200

    Create handles and useState for form

--- client/components/Form.tsx
+++ client/components/Form.tsx
@@ -1,23 +1,43 @@
+import { useState } from "react"
+
 export default function Form() {
+
+  const [formData, setFormData] = useState({
+    name: '',
+    title: '',
+    role: '',
+    dob: '',
+    allergies: ''
+  })
+
+  function handleChange(e: React.FormEvent<HTMLFormElement>) {
+    setFormData({...formData, [e.target.id]: e.target.value})
+  }
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    console.log(formData)
+  }
+
   return (
     <div className="form-container">
       <h1>Add a new employee</h1>
-      <form className="employee-form">
+      <form onChange={handleChange} onSubmit={handleSubmit} className="employee-form">
         <label htmlFor="name">Full Name:</label>
-        <input type="text" id="name"></input>
+        <input type="text" id="name" value={formData.name}></input>
         <label htmlFor="title">Title:</label>
-        <input id="title"></input>
+        <input id="title" value={formData.title}></input>
         <label htmlFor="role">Role:</label>
-        <select id="role" name="role">
+        <select id="role" name="role" value={formData.role}>
           <option value="Super Admin">Super Admin</option>
           <option value="Admin">Admin</option>
           <option value="Employee">Employee</option>
           <option value="Guest">Guest</option>
         </select>
         <label htmlFor="dob">DOB:</label>
-        <input type="date" id="dob"></input>
+        <input type="date" id="dob" value={formData.dob}></input>
         <label htmlFor="allergies">Allergies:</label>
-        <input id="allergies" type="text"></input>
+        <input id="allergies" type="text" value={formData.allergies}></input>
         <input type="submit" value="Submit"></input>
       </form>
     </div>

f7edcf66a3d8477239a81f05dcd9c1ac617cf285
achoo-o <amyj.etana@gmail.com>
2024-08-31 15:26:27 +1200

    q ;/

--- client/components/Form.tsx
+++ client/components/Form.tsx
@@ -1,7 +1,23 @@
 export default function Form() {
   return (
     <div>
-      meow
+      <form>
+        <label htmlFor="name">Full Name:</label>
+        <input type="text" id="name"></input>
+        <label htmlFor="title">Title:</label>
+        <input id="title"></input>
+        <label htmlFor="role">Role:</label>
+        <select id="role" name="role">
+          <option value="Super Admin">Super Admin</option>
+          <option value="Admin">Admin</option>
+          <option value="Employee">Employee</option>
+          <option value="Guest">Guest</option>
+        </select>
+        <label htmlFor="dob">DOB:</label>
+        <input type="date" id="dob"></input>
+        <label htmlFor="allergies">Allergies:</label>
+        <input id="allergies" type="text"></input>
+      </form>
     </div>
   )
 }

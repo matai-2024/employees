491ec8cda52c1ce4c48c5bda06dad1b62da4ed3a
achoo-o <amyj.etana@gmail.com>
2024-09-01 18:16:24 +1200

    Dokkusetup?

--- client/components/Employees.tsx
+++ client/components/Employees.tsx
@@ -1,4 +1,5 @@
 import { useEmployees } from "../hooks/useEmployees";
+import { IfAuthenticated } from "./Authenticated";
 import Loading from "./Loading";
 
 export default function Employees() {
@@ -16,6 +17,15 @@ export default function Employees() {
     )
   }
 
+  function handleDelete(e:  React.MouseEvent<HTMLButtonElement>) {
+    //In future, I want Super Admins ONLY be able to delete. For now, just me is fine
+    console.log(e.target.name)
+  }
+
+  function handleEdit(e:  React.MouseEvent<HTMLButtonElement>) {
+    
+  }
+
   return (
     <section>
         { data.map((employee) => {
@@ -23,12 +33,16 @@ export default function Employees() {
           <div className='employee-container' key={employee.id}>
             <img src={`../../images/${employee.id}.png`} alt="user"></img>
             <div>
-            <h6>{employee.name}</h6>
-            <p>Title: {employee.title}</p>
-            <p>Role: {employee.role}</p>
-            <p>DOB: {String(employee.dob)}</p>
-            <p>Allergies: {employee.allergies.join(', ')}</p>
+              <h6>{employee.name}</h6>
+              <p>Title: {employee.title}</p>
+              <p>Role: {employee.role}</p>
+              <p>DOB: {String(employee.dob)}</p>
+              <p>Allergies: {employee.allergies.join(', ')}</p>
             </div>
+            <IfAuthenticated>
+              <button onClick={handleEdit} name={String(employee.id)}>Edit</button>
+              <button onClick={handleDelete} name={String(employee.id)}>Delete</button>
+            </IfAuthenticated>
           </div>
         )}) }
     </section>

b4593974e0db7dc95a2b10495305302920619d61
achoo-o <amyj.etana@gmail.com>
2024-09-01 15:51:54 +1200

    Add sign in/sign out function

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -1,6 +1,19 @@
+import { useAuth0 } from '@auth0/auth0-react'
+import { IfAuthenticated, IfNotAuthenticated } from './Authenticated.tsx'
+import { NavGroup, NavButton } from './Styled.tsx'
 import { Link } from "react-router-dom";
 
 export default function Loading() {
+  const { user, logout, loginWithRedirect } = useAuth0()
+  
+  const handleSignOut = () => {
+    logout()
+  }
+
+  const handleSignIn = () => {
+    loginWithRedirect()
+  }
+
   return (
     <section className="nav">
       <ul>
@@ -14,6 +27,19 @@ export default function Loading() {
             Add new employee
           </Link>
         </li>
+        <NavGroup>
+        <IfAuthenticated>
+          <NavButton onClick={handleSignOut}>Sign out</NavButton>
+          {user && (
+            <p>
+              Signed in as: {user.nickname}
+            </p>
+          )}
+        </IfAuthenticated>
+        <IfNotAuthenticated>
+          <NavButton onClick={handleSignIn}>Sign in</NavButton>
+        </IfNotAuthenticated>
+      </NavGroup>
       </ul>
     </section>
   )
--- client/index.tsx
+++ client/index.tsx
@@ -1,4 +1,5 @@
 import { createRoot } from 'react-dom/client'
+import { Auth0Provider } from '@auth0/auth0-react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { router } from './router.tsx'
@@ -8,9 +9,18 @@ const queryClient = new QueryClient()
 
 document.addEventListener('DOMContentLoaded', () => {
   createRoot(document.getElementById('app') as HTMLElement).render(
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    <Auth0Provider
+    domain="dev-matai-2024-amy.au.auth0.com"
+    clientId="L7YEwAsNS5EJcx7VlKnvx0F0fkAE6iQe"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+      audience: 'https://employees/api',
+    }}
+    >
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </Auth0Provider>
   )
 })

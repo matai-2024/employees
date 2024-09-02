c17a493623b13c1f6a232e4e72e3ef311690842c
achoo-o <amyj.etana@gmail.com>
2024-09-02 14:51:46 +1200

    Ensure form contents are not empty

--- client/components/Form.tsx
+++ client/components/Form.tsx
@@ -10,6 +10,13 @@ export default function Form() {
     dob: '',
     allergies: ''
   })
+  const [helperText, setHelperText] = useState({
+    name: '',
+    title: '',
+    role: '',
+    dob: '',
+    allergies: ''
+  })
 
   const addEmployee = useAddEmployee()
 
@@ -19,18 +26,36 @@ export default function Form() {
 
   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault()
-    console.log(formData)
-    addEmployee.mutate(formData)
+    let isValid = true
+    //Check if any are empty
+    for (const [key, value] of Object.entries(formData)) {
+      switch(value) {
+        case '':
+          helperText[key] = `*This section is required.`
+          isValid = false
+          break;
+        default: 
+          helperText[key] = ''
+      }
+    }
+
+    if (!isValid) setHelperText({...helperText}) //Re-render
+    if (isValid) addEmployee.mutate(formData) //Send to db
   }
 
   return (
     <div className="form-container">
       <h1>Add a new employee</h1>
       <form onChange={handleChange} onSubmit={handleSubmit} className="employee-form">
+
         <label htmlFor="name">Full Name:</label>
         <input type="text" id="name" value={formData.name}></input>
+        <p className="helper-text">{helperText.name}</p>
+
         <label htmlFor="title">Title:</label>
         <input id="title" value={formData.title}></input>
+        <p className="helper-text">{helperText.title}</p>
+
         <label htmlFor="role">Role:</label>
         <select id="role" name="role" value={formData.role}>
           <option value="Super Admin">Super Admin</option>
@@ -38,10 +63,16 @@ export default function Form() {
           <option value="Employee">Employee</option>
           <option value="Guest">Guest</option>
         </select>
+        <p className="helper-text">{helperText.role}</p>
+
         <label htmlFor="dob">DOB:</label>
         <input type="date" id="dob" value={formData.dob}></input>
+        <p className="helper-text">{helperText.dob}</p>
+
         <label htmlFor="allergies">Allergies:</label>
         <input id="allergies" type="text" value={formData.allergies}></input>
+        <p className="helper-text">{helperText.allergies}</p>
+
         <input type="submit" value="Submit"></input>
       </form>
     </div>

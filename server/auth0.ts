1eb10ec76e15886528e1e898aef65b729a17c649
Amy Jackson <amyj.etana@gmail.com>
2024-09-03 11:56:08 +1200

    Setup Dokku

--- server/auth0.ts
+++ server/auth0.ts
@@ -5,8 +5,8 @@ import { JwtPayload } from 'jsonwebtoken'
 import jwks from 'jwks-rsa'
 
 // TODO: set the domain and audience (API Identifier)
-const domain = 'https://'
-const audience = 'https://'
+const domain = 'https://dev-matai-2024-amy.au.auth0.com'
+const audience = 'https://employees/api'
 
 const checkJwt = jwt({
   secret: jwks.expressJwtSecret({
